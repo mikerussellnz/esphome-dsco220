1421f4c5d0463f63f82f4062c1bbd22dccdff4b9
Mike Russell <mikerussellnz@gmail.com>
2026-09-20 17:54:43 +0800

    updating polling interval to 10s for faster updates.

--- components/dsco220/sensor.py
+++ components/dsco220/sensor.py
@@ -18,7 +18,7 @@ CONFIG_SCHEMA = (
         icon=ICON_MOLECULE_CO2,
         accuracy_decimals=0,
     )
-    .extend(cv.polling_component_schema("60s"))
+    .extend(cv.polling_component_schema("10s"))
     .extend(uart.UART_DEVICE_SCHEMA)
 )
 
